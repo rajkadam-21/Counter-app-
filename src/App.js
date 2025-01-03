556d3ce48483985521a34fa8bc2d31b39b14d8a8
rajkadam-21 <merajkadam@gmail.com>
2025-01-03 13:19:59 +0530

    second day commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const[count,setCount]= useState(0)
+
+function subHandler(){
+setCount(count-1)
+}
+
+  function addHandler(){
+setCount(count+1)
+  }
+
+  function resetHandler(){
+setCount(0)
+  }
   return (
 
-  <div>
-    hello world
-  </div>
+ <div className='container'>
+
+<button onClick={subHandler} id='sub'>
+  -
+</button>
+
+<div>
+  <h1>{count}</h1>
+</div>
+
+<button onClick={addHandler} id='add'>
+  +
+</button>
+<br>
+</br>
+
+<button onClick={resetHandler} id='reset'>
+  reset
+</button>
+<br>
+</br>
+
+ </div>
 
   );
 }
